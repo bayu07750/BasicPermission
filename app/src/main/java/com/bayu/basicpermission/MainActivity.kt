c092aa85da3ecf4a08810f6dda2e7c97735e1776
bayu07750 <bayu07750@gmail.com>
2022-02-04 10:01:25 +0700

    mendeklrasikan izin

--- app/src/main/java/com/bayu/basicpermission/MainActivity.kt
+++ app/src/main/java/com/bayu/basicpermission/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.bayu.basicpermission
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val isDeviceHasCameraFront = checkDeviceHasCemaraFront()
+    }
+
+    private fun checkDeviceHasCemaraFront(): Boolean {
+        return applicationContext.packageManager.hasSystemFeature(PackageManager.FEATURE_CAMERA_FRONT)
     }
 }
